774e5c02344503b97554b5f3d3867ba124d4318f
cesarmosqueira <cesarmanuel.2802@gmail.com>
2021-11-14 19:17:34 -0500

    Fix base api route

--- src/api/api.businesses.tsx
+++ src/api/api.businesses.tsx
@@ -2,11 +2,11 @@ import request from "./api";
 import { Business }  from "../models/business";
 
 const apiBusinesses = {
-  list: () => request.get<Business[]>("/Business"),
-  add: (data: Business) => request.post("/Business", data),
-  edit: (data: Business) => request.put(`/Business/${data.id}`, data),
-  delete: (id: number) => request.delete(`/Business/${id}`),
-  detail: (id: string) => request.get<Business>(`/Business/${id}`),
+  list: () => request.get<Business[]>("/v1/Business"),
+  add: (data: Business) => request.post("/v1/Business", data),
+  edit: (data: Business) => request.put(`/v1/Business/${data.id}`, data),
+  delete: (id: number) => request.delete(`/v1/Business/${id}`),
+  detail: (id: string) => request.get<Business>(`/v1/Business/${id}`),
 };
 
 export default apiBusinesses;
