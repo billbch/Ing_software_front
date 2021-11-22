cfbe07a785829b24386773b2da319d829b9a17cc
SebsPER <67677649+SebsPER@users.noreply.github.com>
2021-11-22 01:17:27 -0500

    Add files via upload

--- src/api/api.pet.tsx
+++ src/api/api.pet.tsx
@@ -1,9 +1,10 @@
 import { Pet } from "../models/pet-form";
+import { Petcreate } from "../models/pet-form-create";
 import request from "./api";
 
 const apiPet = {
   list: () => request.get<Pet[]>("/v1/Pet"),
-  add: (data: Pet) => request.post("/v1/Pet", data),
+  add: (data: Petcreate) => request.post(`/v1/Pet`, data),
   edit: (data: Pet) => request.put(`/v1/Pet/${data.id}`, data),
   delete: (id: number) => request.delete(`/v1/Pet/${id}`),
   detail: (id: string) => request.get<Pet>(`/v1/Pet/${id}`),
