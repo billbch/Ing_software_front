25c77849cb55bfc2cb6b2460af2f4c9ebbc69181
SebsPER <67677649+SebsPER@users.noreply.github.com>
2021-11-22 01:17:53 -0500

    Add files via upload

---
+++ src/models/pet-form-create.ts
@@ -0,0 +1,11 @@
+export class Petcreate{
+    id: number=0;
+    name: string='';
+    lastName: string='';
+    birthDate: Date;
+    age: number=0;
+    breed: string='';
+    photo: string='';
+    gender: boolean;
+    personProfileId: number=0;
+}
