9bbc6087ef738465c2baf740d7189f82071b043a
SebsPER <67677649+SebsPER@users.noreply.github.com>
2021-11-21 22:55:38 -0500

    fix crasheo de appointment list

--- src/pages/appointmentVet/appointmentVet-list.tsx
+++ src/pages/appointmentVet/appointmentVet-list.tsx
@@ -110,9 +110,12 @@ const theme = createMuiTheme({palette: {primary: red, secondary: green, error: r
   function fillPets() {
     let vals:number[] = [];
     for (let i = 0; i < appointments.length; i++) {
-      let pn = pets.filter((x) => x.personProfileId == appointments[i].personProfileId);
-      //console.log(vals);
-      vals.push(pn[0].id);
+      let pn:Pet[] = pets.filter((x) => x.personProfileId == appointments[i].personProfileId);
+      if (pn[0] != null){
+        console.log(pn[0].id);
+        let n:number = pn[0].id;
+        vals.push(n);
+      } else {vals.push(1);}
     }
     //console.log(vals);
     setPetIds(vals);
@@ -149,7 +152,6 @@ const theme = createMuiTheme({palette: {primary: red, secondary: green, error: r
                       <TableCell>Nro</TableCell>
                       <TableCell>Fecha</TableCell>
                       <TableCell>Veterinaria</TableCell>
-                      <TableCell>Dueño</TableCell>
                       <TableCell>Mascota</TableCell>
                       <TableCell>Estado</TableCell>
                       <TableCell>Eliminar</TableCell>
@@ -161,7 +163,6 @@ const theme = createMuiTheme({palette: {primary: red, secondary: green, error: r
                         <TableCell>{index + 1}</TableCell>
                         <TableCell>{appointment.startTime}</TableCell>
                         <TableCell>{appointment.veteryname}</TableCell>
-                        <TableCell>{perNames[index]}</TableCell>
                         <TableCell>
                           <Button onClick={()=> setpetId(petIds[index])}     
                             component={Link}
